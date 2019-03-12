f94e8dd7375f69d0ca21582f07bef2190a3a8d3a
Thushar Tom <thushartom@gmail.com>
2019-03-12 07:42:46 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <head></head>
+    <body>
+        <form action="index.php" method="post">
+            Enter your Post<input name="post" type="text">
+            <input type="submit">
+        </form>
+
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Principal Talks</title>
+        <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div> <img class="profile" src="./Resources/img_avatar3.png" alt="Avatar"> </div>
+
+        <div class="block2"></div>
+        <div class="block1"></div>
+        <h1>Principal<br>Talks</h1>
+        <center><h6>College of Engineering Chengannur</h6></center>
+        <img class="mic" src="./Resources/mic.png" alt="mic" >
+        <h2 class="H1">Highlights</h2>
+        <h2 class="H2">History</h2>
+       <div class="chatBox"><br>Hello Guys!!<br>I am so glad I can talk to you!<br>I have converted some text to outlines and want to give the text some rough edging to give it an organic feel.
+ An example of the effect I would like to achieve is shown on the image below.
+ What is the best way to achieve / create this kind of effect?</div>
+       
+       <div class="chatboxH1">Hello Guys!!<br>I am so glad I can talk to you!<br>I have converted some text to outlines and want to give the text some rough edging to give it an organic feel.
+    An example of the effect I would like to achieve is shown on the image below.
+    What is the best way to achieve / create this kind of effect?</div>
+        <div class="chatboxH2"><p>we'll all be living in some sci-fi realm where health care is less complicated and robots take care of the elderly. But in the real world, enjoying your golden years requires careful planning long before you reach them.</p></div>
+
+    </body>
+
+
+</html>
