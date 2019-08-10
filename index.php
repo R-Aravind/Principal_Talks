bdca6ac8cf1d5be8bcfab8c6df05d633d0576ef9
Akhil Ravindran <imakhilravindran@gmail.com>
2019-08-10 10:29:50 +0530

    create header design

--- index.php
+++ index.php
@@ -15,14 +15,9 @@
         <img class="mic" src="./resources/mic.png" alt="mic" >
         <h2 class="H1">Highlights</h2>
         <h2 class="H2">History</h2>
-       <div class="chatBox"><br>Hello Guys!!<br>I am so glad I can talk to you!<br>I have converted some text to outlines and want to give the text some rough edging to give it an organic feel.
- An example of the effect I would like to achieve is shown on the image below.
- What is the best way to achieve / create this kind of effect?</div>
-
-       <div class="chatboxH1">Hello Guys!!<br>I am so glad I can talk to you!<br>I have converted some text to outlines and want to give the text some rough edging to give it an organic feel.
-    An example of the effect I would like to achieve is shown on the image below.
-    What is the best way to achieve / create this kind of effect?</div>
-        <div class="chatboxH2"><p>we'll all be living in some sci-fi realm where health care is less complicated and robots take care of the elderly. But in the real world, enjoying your golden years requires careful planning long before you reach them.</p></div>
+        <div class="chatBox"><br>Hello Guys!!<br>I am so glad I can talk to you!<br>I have converted some text to outlines and want to give the text some rough edging to give it an organic feel.
+          An example of the effect I would like to achieve is shown on the image below.
+            What is the best way to achieve / create this kind of effect?</div>
 
     </body>
 
