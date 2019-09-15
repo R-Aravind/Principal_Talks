46c4a5208cfc6928189c595c6df45a55294d6079
imakhil <imakhilravindran@gmail.com>
2019-09-15 10:58:31 +0530

    Add hover animation to card like

---
+++ resources/js/main.ui.js
@@ -0,0 +1,8 @@
+// // DOM Elements
+// const $like_btn = $('.card-like');
+
+
+// // Hover Like Button
+// $like_btn.hover(()=>{
+// 	$(this+' img').src('/resources/images/clap_active.svg');
+// });
